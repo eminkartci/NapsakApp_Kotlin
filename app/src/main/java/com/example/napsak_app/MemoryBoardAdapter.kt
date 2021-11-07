9058711b2a0992e925057b6d1602bfeb421142aa
eminkartci <emin.kartci@ozu.edu.tr>
2021-11-07 08:21:20 +0300

    Activity Board comments

--- app/src/main/java/com/example/napsak_app/MemoryBoardAdapter.kt
+++ app/src/main/java/com/example/napsak_app/MemoryBoardAdapter.kt
@@ -6,24 +6,41 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
+// Activity Board 
 class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
+    // overwrite ViewHolder
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val cardWidth = parent.width /2
-        val cardHeight = parent.height /2
-        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
-        return ViewHolder(view)
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+
+        // constructor
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            // set the width and height of the cards
+            val cardWidth = parent.width /2
+            val cardHeight = parent.height /2
+
+            // initialize the view
+            val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+
+            // return ViewHolder
+            return ViewHolder(view)
+        }
+
+        // This happens exactly in onBindViewHolder().
+        // Initially you will get new unused view holders and you have to
+        // fill them with data you want to display.
+        // But as you scroll you'll start getting view holders
+        // that were used for rows that went off screen
+        // and you have to replace old data that they held with new data.
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            holder.bind(position)
+            // No-op
+        }
+
+        override fun getItemCount() = numPieces
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        holder.bind(position)
-        // No-op
-    }
-
-    override fun getItemCount() = numPieces
-    }
 
 private fun RecyclerView.ViewHolder.bind(position: Int) {
 
