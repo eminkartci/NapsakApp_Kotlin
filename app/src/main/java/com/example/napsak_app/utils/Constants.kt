1700ec242cfbf101b100233062096199bfe17c7e
eminkartci <emin.kartci@ozu.edu.tr>
2021-12-03 14:40:56 +0300

    Main Activity Fragment Yapılacak

--- app/src/main/java/com/example/napsak_app/utils/Constants.kt
+++ app/src/main/java/com/example/napsak_app/utils/Constants.kt
@@ -3,7 +3,6 @@ package com.example.napsak_app.utils
 import com.example.napsak_app.R
 
 // The Data of the card front page with Icons it will be changed
-
 val DEFAULT_ICONS = listOf(
     R.drawable.books,
     R.drawable.coding,
