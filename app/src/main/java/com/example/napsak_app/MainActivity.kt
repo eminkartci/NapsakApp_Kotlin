bf33978fabce313aa49891b7fa7baae37b159a90
eminkartci <emin.kartci@ozu.edu.tr>
2021-12-07 18:43:08 +0300

    Event Grid can be arranged now

--- app/src/main/java/com/example/napsak_app/MainActivity.kt
+++ app/src/main/java/com/example/napsak_app/MainActivity.kt
@@ -14,11 +14,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.cardview.widget.CardView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
@@ -76,16 +79,17 @@ class MainActivity : AppCompatActivity() {
 
         cvRefresh.setOnClickListener(clickListener)
 
-        // The number of copies in here there is 3 copy because of less card drafts
-        val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
-        val randomizedImages = (chosenImages + chosenImages + chosenImages).shuffled()
-        // Defining memoryCards
-        // val memoryCards = randomizedImages.map{ NapsakCard(it) }
+
 
         setEvents()
     }
 
     private fun setEvents() {
+        // The number of copies in here there is 3 copy because of less card drafts
+        val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumPairs())
+        val randomizedImages = (chosenImages + chosenImages + chosenImages).shuffled()
+        // Defining memoryCards
+        // val memoryCards = randomizedImages.map{ NapsakCard(it) }
         memoryGame = MemoryGame(boardSize)
         // Recycler view adapter: The Adapter provides access to the data items.
         adapter = NapsakBoardAdapter(this,boardSize,memoryGame.cards, object: NapsakBoardAdapter.CardClickListener{
@@ -127,6 +131,10 @@ class MainActivity : AppCompatActivity() {
                 val personalTestIntent = Intent(mainContext,PersonalTestActivity::class.java)
                 startActivity(personalTestIntent)
             }
+
+            R.id.im_change_size  -> {
+                changeActivityGrid()
+            }
         }
 
         return super.onOptionsItemSelected(item)
@@ -141,6 +149,47 @@ class MainActivity : AppCompatActivity() {
                 setEvents()
             }
         }
+    }
+
+    // to change activity count and grid
+    private fun changeActivityGrid(){
+        // get the board size variable
+        val boardSizeView: View = LayoutInflater.from(mainContext).inflate(R.layout.dialog_board_size,null)
+        val radioBtnGroup = boardSizeView.findViewById<RadioGroup>(R.id.rgEventGrid)
+
+        // set default check
+        when(boardSize){
+            BoardSize.EASY -> radioBtnGroup.check(R.id.rbMinimal)
+            BoardSize.MEDIUM -> radioBtnGroup.check(R.id.rbMiddle)
+            BoardSize.HARD -> radioBtnGroup.check(R.id.rbMany)
+        }
+
+        // show alert dialog to interact with the user
+        showAlertDialog("More Events?","No Thanks","Sure!",boardSizeView,View.OnClickListener {
+            boardSize = when (radioBtnGroup.checkedRadioButtonId) {
+                R.id.rbMinimal -> BoardSize.EASY
+                R.id.rbMiddle -> BoardSize.MEDIUM
+                R.id.rbMany -> BoardSize.HARD
+                else -> {
+                    BoardSize.MEDIUM
+                }
+            }
+            Log.i(TAG,"New Board Size: $boardSize")
+            setEvents()
+        })
+
+
+    }
+
+    // Create an alert dialog function
+    private fun showAlertDialog(title: String, cancelStr: String,acceptStr: String,view: View?, positiveClickListener: View.OnClickListener) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton(cancelStr, null)
+            .setPositiveButton(acceptStr) { _, _ ->
+                positiveClickListener.onClick(null)
+            }.show()
     }
 }
 
