271eb73873a144c2a8f11e41bf42b9a8ff63dcf6
eminkartci <emin.kartci@ozu.edu.tr>
2021-12-07 20:05:11 +0300

    Back icon is added to the Activity Even

--- app/src/main/java/com/example/napsak_app/EventActivity.kt
+++ app/src/main/java/com/example/napsak_app/EventActivity.kt
@@ -2,10 +2,23 @@ package com.example.napsak_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class EventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event)
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == android.R.id.home){
+            finish()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
