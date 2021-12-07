dafb07e9ede3e512c5f83732e0fc23c63ac272e9
eminkartci <emin.kartci@ozu.edu.tr>
2021-12-07 23:25:15 +0300

    New Event Activity is designing

--- app/src/main/java/com/example/napsak_app/NewEventActivity.kt
+++ app/src/main/java/com/example/napsak_app/NewEventActivity.kt
@@ -2,10 +2,38 @@ package com.example.napsak_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import androidx.databinding.DataBindingUtil
+import com.example.napsak_app.databinding.ActivityMainBinding
+import com.example.napsak_app.databinding.ActivityNewEventBinding
 
 class NewEventActivity : AppCompatActivity() {
+
+    // Variables
+    private lateinit var cvNewImage : CardView
+    private lateinit var ivNewImage : ImageView
+    private lateinit var btnShare : Button
+    private lateinit var etNewEventTitle : EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_event)
+
+
+        // Binding
+        val newEventBinding: ActivityNewEventBinding = DataBindingUtil.setContentView(this,R.layout.activity_new_event)
+        //setContentView(R.layout.activity_new_event)
+
+        // Assign
+        cvNewImage = newEventBinding.cvNewImage
+        ivNewImage = newEventBinding.ivNewEventImage
+        etNewEventTitle = newEventBinding.etEventTitle
+        btnShare = newEventBinding.btnShareEvent
+
+        
+
     }
 }
