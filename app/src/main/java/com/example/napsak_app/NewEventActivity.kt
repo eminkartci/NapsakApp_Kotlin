4acd0cf0fdb2e2d63b9cfd00e821e24bb17b894c
eminkartci <emin.kartci@ozu.edu.tr>
2021-12-09 20:18:54 +0300

    Personal Test is added

--- app/src/main/java/com/example/napsak_app/NewEventActivity.kt
+++ app/src/main/java/com/example/napsak_app/NewEventActivity.kt
@@ -1,7 +1,9 @@
 package com.example.napsak_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -11,6 +13,10 @@ import com.example.napsak_app.databinding.ActivityMainBinding
 import com.example.napsak_app.databinding.ActivityNewEventBinding
 
 class NewEventActivity : AppCompatActivity() {
+
+    companion object{
+        private const val PICK_PHOTO_CODE = 655
+    }
 
     // Variables
     private lateinit var cvNewImage : CardView
@@ -22,6 +28,8 @@ class NewEventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // home icon show
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
 
         // Binding
         val newEventBinding: ActivityNewEventBinding = DataBindingUtil.setContentView(this,R.layout.activity_new_event)
@@ -33,7 +41,22 @@ class NewEventActivity : AppCompatActivity() {
         etNewEventTitle = newEventBinding.etEventTitle
         btnShare = newEventBinding.btnShareEvent
 
-        
+        cvNewImage.setOnClickListener({
+            val getImageIntent = Intent(Intent.ACTION_PICK)
+            getImageIntent.type = "image/*"
+            startActivityForResult(Intent.createChooser(getImageIntent,"Please select an event image."),PICK_PHOTO_CODE)
+        })
 
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == android.R.id.home){
+            finish()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
--- app/src/main/java/com/example/napsak_app/PersonalTestActivity.kt
+++ app/src/main/java/com/example/napsak_app/PersonalTestActivity.kt
@@ -2,10 +2,27 @@ package com.example.napsak_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class PersonalTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_test)
+
+        // home icon show
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+
+        // update the title
+        supportActionBar?.title = "Take Personal Test"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == android.R.id.home){
+            finish()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
