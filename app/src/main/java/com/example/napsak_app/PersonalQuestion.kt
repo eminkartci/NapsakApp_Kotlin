73d1255eae7583c93cfc34083f589e62801f14f4
eminkartci <emin.kartci@ozu.edu.tr>
2021-12-12 01:58:33 +0300

    Add question class

---
+++ app/src/main/java/com/example/napsak_app/PersonalQuestion.kt
@@ -0,0 +1,8 @@
+package com.example.napsak_app
+
+data class PersonalQuestion (
+    val ID : Int,
+    val question: String,
+    val Image: Int,
+    val answer: Int
+)
--- app/src/main/java/com/example/napsak_app/utils/Constants.kt
+++ app/src/main/java/com/example/napsak_app/utils/Constants.kt
@@ -1,5 +1,7 @@
 package com.example.napsak_app.utils
 
+import android.app.Person
+import com.example.napsak_app.PersonalQuestion
 import com.example.napsak_app.R
 
 // The Data of the card front page with Icons it will be changed
@@ -15,3 +17,23 @@ val DEFAULT_ICONS = listOf(
     R.drawable.podcast,
     R.drawable.youtube
 )
+
+object Constants{
+
+    // get questions
+    fun getPersonalQuestions(): ArrayList<PersonalQuestion>{
+        val personalQuestionList = ArrayList<PersonalQuestion>()
+
+        val timeQuestion = PersonalQuestion(1,"Do you have time?",R.drawable.music,-1)
+        val socialQuestion = PersonalQuestion(1,"Do you want to socialize?",R.drawable.chatting,-1)
+        val selfImprovementQuestion = PersonalQuestion(1,"Do you want to improve yourself?",R.drawable.language,-1)
+        val physicalQuestion = PersonalQuestion(1,"Do you want to do physical activity?",R.drawable.walking,-1)
+
+        personalQuestionList.add(timeQuestion)
+        personalQuestionList.add(socialQuestion)
+        personalQuestionList.add(selfImprovementQuestion)
+        personalQuestionList.add(physicalQuestion)
+
+        return personalQuestionList
+    }
+}
