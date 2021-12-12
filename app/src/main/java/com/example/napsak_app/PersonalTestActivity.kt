307033a07173918832fdb788d33a3502e1576ab9
eminkartci <emin.kartci@ozu.edu.tr>
2021-12-12 11:49:34 +0300

    Questions are added by using binding

--- app/src/main/java/com/example/napsak_app/PersonalTestActivity.kt
+++ app/src/main/java/com/example/napsak_app/PersonalTestActivity.kt
@@ -2,18 +2,51 @@ package com.example.napsak_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.napsak_app.databinding.ActivityMainBinding
+import com.example.napsak_app.databinding.ActivityPersonalTestBinding
+import com.example.napsak_app.utils.Constants
 
 class PersonalTestActivity : AppCompatActivity() {
+
+    private var currentQuestionIndex : Int = 0
+    private lateinit var personalQuestions: ArrayList<PersonalQuestion>
+
+    private lateinit var personalTestBinding: ActivityPersonalTestBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_personal_test)
+
+        personalTestBinding= DataBindingUtil.setContentView(this,R.layout.activity_personal_test)
+        //setContentView(R.layout.activity_personal_test)
 
         // home icon show
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
 
         // update the title
         supportActionBar?.title = "Take Personal Test"
+
+        // get questions
+        personalQuestions = Constants.getPersonalQuestions()
+        
+        // get the first question
+        nextQuestion()
+        
+        // add click listeneter
+        personalTestBinding.btnSubmit.setOnClickListener {
+            if (isAnswered(personalQuestions.get(currentQuestionIndex))){
+                nextQuestion()
+            }else{
+                Toast.makeText(this,"Please answer the question.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        
+        
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -25,4 +58,17 @@ class PersonalTestActivity : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
+
+    private fun nextQuestion(){
+        // Log.i("Question Size: ", "${personalQuestions.size}")
+        personalTestBinding.tvPqQuestion.text = personalQuestions.get(currentQuestionIndex).question
+
+        currentQuestionIndex += 1
+    }
+
+    private fun isAnswered(pq : PersonalQuestion): Boolean {
+        return pq.answer != -1
+    }
+
+
 }
