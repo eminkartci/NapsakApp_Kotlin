d8cf3492d754de96312f02d4090f9267d78d32c5
eminkartci <emin.kartci@ozu.edu.tr>
2021-12-25 22:12:25 +0300

    Youtube intent is successfull

--- app/src/main/java/com/example/napsak_app/ActivitySelect.kt
+++ app/src/main/java/com/example/napsak_app/ActivitySelect.kt
@@ -5,12 +5,21 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import com.example.napsak_app.models.Event
 import com.example.napsak_app.models.NapsakCard
+import android.content.ActivityNotFoundException
+import android.content.Context
+
+import android.content.Intent
+import android.net.Uri
+
 
 class ActivitySelect : AppCompatActivity() {
 
     private lateinit var declineBtn: Button
+    private lateinit var acceptBtn: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,13 +29,25 @@ class ActivitySelect : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true);
 
         declineBtn = findViewById(R.id.btn_decline)
+        acceptBtn = findViewById(R.id.btn_accept)
+
         declineBtn.setOnClickListener {
             finish()
         }
 
+
         val tempActivity = intent.getSerializableExtra("activity") as Event
         Log.i("S Activity:", tempActivity.toString())
         //val tempActivity = Event(0,"Coding",R.drawable.cinema,5,4,3,2)
+
+        acceptBtn.setOnClickListener {
+            Toast.makeText(this, "${tempActivity.eventTitle} is added to your list!",Toast.LENGTH_LONG).show();
+            if (tempActivity.eventImage == R.drawable.youtube){
+                watchYoutubeVideo(this,"ZMEFZpQABBg");
+            }
+
+            finish()
+        }
 
         // update the title
         supportActionBar?.title = tempActivity.eventTitle
@@ -53,4 +74,18 @@ class ActivitySelect : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
+
+    fun watchYoutubeVideo(context: Context, id: String) {
+        val appIntent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:$id"))
+        val webIntent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("http://www.youtube.com/watch?v=$id")
+        )
+        try {
+            context.startActivity(appIntent)
+        } catch (ex: ActivityNotFoundException) {
+            context.startActivity(webIntent)
+        }
+    }
+
 }
